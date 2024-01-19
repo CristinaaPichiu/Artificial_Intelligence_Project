d37c64bc9edf0f7d85493c1501bf554e40e6e074
Cristina Pichiu <cristinapichiu38@gmail.com>
2024-01-19 21:12:36 +0200

    prophet update

--- SensorML/prophet_file.py
+++ SensorML/prophet_file.py
@@ -3,6 +3,9 @@ from prophet import Prophet
 import matplotlib.pyplot as plt
 import os
 
+from SensorML.read_csv import load_and_clean_data
+
+
 def plot_actual_vs_predicted(df, forecast, parameter):
     plt.figure(figsize=(16, 6))
     plt.plot(df['Timestamp'], df[parameter], label='Actual', color='blue')
@@ -41,10 +44,10 @@ def return_path_prophet(column):
 
 if __name__ == "__main__":
     file_path = 'SensorMLDataset.csv'
-    df = pd.read_csv(file_path)
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    df = load_and_clean_data(file_path)
 
-    parameters = ['pres', 'temp1', 'umid', 'temp2', 'V450', 'B500', 'G550', 'Y570', 'O600', 'R650', 'temps1', 'temps2', 'lumina']
+    parameters = ['pres', 'temp1', 'umid', 'temp2', 'V450', 'B500', 'G550', 'Y570', 'O600', 'R650', 'temps1', 'temps2',
+                  'lumina']
     prophet_models = {}
     for parameter in parameters:
         prophet_models[parameter] = train_prophet_model_for_parameter(df, parameter)
